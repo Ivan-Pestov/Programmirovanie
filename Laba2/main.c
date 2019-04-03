4980e4993cd320069f19cd2630840372af926dbe
Ivan-Pestov <47943361+Ivan-Pestov@users.noreply.github.com>
2019-04-03 12:58:20 +0300

    Update main.c

--- Laba2/main.c
+++ Laba2/main.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 int main()
 {
-	float s;
+    float s;
 	float a;
 	float eps;
 	int i;
 	int sign;
-  
-	printf("eps=");
+
+	printf_s("eps=\n");
 	scanf_s("%f", &eps);
-  
-	s = 0;
+	s = 0.0;
 	sign = 1;
-	i = 1;
-	a = (i + 2) / (i*(i + 1));
-	s = s + a;
-	i = i + 1;
-  
-	while (a > eps);
+	i = 2;
+	a = 1.5;
+	
+	while (a > eps)
 	{
-		sign = -sign;
-		a = (i + 2) / (i*(i + 1));
+		sign = (-1)*sign;
+		a = (i + 2) / (i*(i + 1.0));
 		s = s + a * sign;
 		i = i + 1;
 	}
-	printf("s=%f", s);
+	printf_s("s=%f\n", s);
 	return 0;
 }
