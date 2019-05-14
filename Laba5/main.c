6f127727ef472eae455cce22a024d3478aef945f
Ivan-Pestov <47943361+Ivan-Pestov@users.noreply.github.com>
2019-05-14 20:52:39 +0300

    Лабораторная работа №5

---
+++ Laba5/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define N 10
+
+int main()
+{
+	int x[N];
+	int i;
+	float q;
+	int F=1;
+	for (i = 0; i < N; i++)
+		scanf_s("%d", &x[i]);
+	q = (float)x[1] / (float)x[0];
+	for (i = 0; i < N-1; i++)
+	{
+		if ((float)(x[i+1]/x[i])!=q)
+			F = 0;
+	}
+	if (F==1)
+		printf("true\n");
+	else
+		printf("false\n");
+	return 0;
+}
